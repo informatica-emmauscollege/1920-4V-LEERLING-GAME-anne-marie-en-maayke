f8c3edd0d808f30285257efccaa280d47a90912e
annemarieb04 <annemariebaart04@gmail.com>
2020-06-17 12:58:36 +0000

    tekenGameOverTekst poging 1
    Signed-off-by: annemarieb04 <annemariebaart04@gmail.com>

--- script.js
+++ script.js
@@ -158,7 +158,9 @@ var tekenGameOverVeld = function() {
 }
 
 var tekenGameOverTekst = function() {
-    
+    textSize(50);
+    fill(255,255,255)
+    text('GAME OVER', 20, 20, 640, 670);
 }
 
 /**
